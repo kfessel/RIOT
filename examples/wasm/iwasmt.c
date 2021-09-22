5b230255eec5dabb47acfc40f692ded1a1f4edbb
Karl Fessel <karl.fessel@ovgu.de>
2021-09-22 20:12:19 +0200

    example/wamr: improve usability of example

--- examples/wasm/iwasmt.c
+++ examples/wasm/iwasmt.c
@@ -27,45 +27,63 @@
 static int app_argc;
 static char **app_argv;
 
-static void* app_instance_main(wasm_module_inst_t module_inst)
+/* execs the main function in an instantiated module */
+static int iwasm_instance_exec_main(wasm_module_inst_t module_inst, int argc, char **argv)
 {
-    const char *exception;
-
-    wasm_application_execute_main(module_inst, app_argc, app_argv);
+    const char *exception = 0;
+    int ret = 0;
+    wasm_application_execute_main(module_inst, argc, argv);
     if ((exception = wasm_runtime_get_exception(module_inst))){
         puts(exception);
+        return -1;
     }
-    return NULL;
+    if(argc > 0) ret = *((int*)argv);
+    return ret;
 }
 
-void * iwasm_t(void *arg1)
+/* execs the main function of a loaded module */
+int iwasm_module_exec_main(wasm_module_t wasm_module , int argc, char **argv)
 {
-    wasm_module_t wasm_module = (wasm_module_t) arg1;
     wasm_module_inst_t wasm_module_inst = NULL;
     char error_buf[128];
+    int ret = 0;
 
     /* instantiate the module */
     if (!(wasm_module_inst = wasm_runtime_instantiate(wasm_module, 8 * 1024,
         8 * 1024, error_buf, sizeof(error_buf)))) {
         puts(error_buf);
-    }else{
-        app_instance_main(wasm_module_inst);
-        /* destroy the module instance */
-        wasm_runtime_deinstantiate(wasm_module_inst);
+        return -1;
     }
-    return NULL;
+    ret = iwasm_instance_exec_main(wasm_module_inst, argc, argv);
+    /* destroy the module instance */
+    wasm_runtime_deinstantiate(wasm_module_inst);
+    return ret;
 }
 
-void * iwasm_main(void *arg1)
-{
-    (void) arg1; /*unused*/
-    uint8_t *wasm_file_buf = NULL;
-    unsigned wasm_file_buf_size = 0;
+/* bytecode needs to be writeable*/
+int iwasm_bytecode_exec_main(void *bytecode, size_t bytecode_len, int argc, char **argv){
+//  (uint8_t *) bytecode;
+    int ret = 0;
     wasm_module_t wasm_module = NULL;
-    char error_buf[128];
 
+    {   /* load WASM module */
+        char error_buf[128];
+        if (!(wasm_module = wasm_runtime_load((uint8_t * )bytecode, bytecode_len,
+                                          error_buf, sizeof(error_buf)))) {
+            puts(error_buf);
+            return -1;
+        }
+    }
+    ret = iwasm_module_exec_main(wasm_module, argc, argv);
+    /* unload WASM module */
+    wasm_runtime_unload(wasm_module);
+    return ret;
+}
+
+/* running this initialises the wamr runtime*/
+bool iwasm_runtime_init(void)
+{
     RuntimeInitArgs init_args;
-
 /* chose allocator non defaults to system allocator */
 #define FUNC_ALLOC
 //#define POOL_ALLOC
@@ -80,6 +98,7 @@ void * iwasm_main(void *arg1)
 #elif defined(FUNC_ALLOC)
     init_args.mem_alloc_type = Alloc_With_Allocator;
 /*deactivate Wpedantic for some lines*/
+#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
     init_args.mem_alloc_option.allocator.malloc_func = malloc;
     init_args.mem_alloc_option.allocator.realloc_func = realloc;
@@ -92,33 +111,37 @@ void * iwasm_main(void *arg1)
     /* initialize runtime environment */
     if (!wasm_runtime_full_init(&init_args)) {
         puts("Init runtime environment failed.");
-        return NULL;
+        return false;
     }
+    return true;
+}
 
-    /* we need the byte codes be writable while loading it adds size information to
-    /* stack POPs */
+void * iwasm_thread(void *arg1)
+{
+    (void) arg1; /*unused*/
+    uint8_t *wasm_buf = NULL;
+    unsigned wasm_buf_size = 0;
+
+    if(!iwasm_runtime_init())
+        return (void *)-1;
+
+    /* we need the bytecode to be writable while loading
+     * loading adds size information to stack POPs */
     //static uint8_t wasm_test_file[] = main_wasm;
-    wasm_file_buf = malloc(main_wasm_len);
-    memcpy(wasm_file_buf, main_wasm, main_wasm_len);
-    /* if architecture has const in writable mem
-    /* wasm_file_buf = (uint8_t *) main_wasm; */
-    wasm_file_buf_size = main_wasm_len;
+    wasm_buf = malloc(main_wasm_len);
+    memcpy(wasm_buf, main_wasm, main_wasm_len);
+    /* no copy need if architecture has const in writable mem */
+    /* wasm_buf = (uint8_t *) main_wasm; */
+    wasm_buf_size = main_wasm_len;
 
-    /* load WASM module */
-    if (!(wasm_module = wasm_runtime_load(wasm_file_buf, wasm_file_buf_size,
-                                          error_buf, sizeof(error_buf)))) {
-        puts(error_buf);
+    iwasm_bytecode_exec_main(wasm_buf, wasm_buf_size, app_argc, app_argv);
 
-    }else{
-        iwasm_t(wasm_module);
-        wasm_runtime_unload(wasm_module);
-    }
-
+    free(wasm_buf);
     wasm_runtime_destroy();
     return NULL;
 }
 
-bool iwasm_init(void)
+bool iwasm_start_thread(void)
 {
     struct{
         char *  stack;
@@ -132,7 +155,7 @@ bool iwasm_init(void)
         .stacksize = DEFAULT_THREAD_STACKSIZE,
         .priority = 8,
         .flags = 0,
-        .task_func = iwasm_main,
+        .task_func = iwasm_thread,
         .arg = NULL,
         .name = "simple_wamr"
     };
@@ -143,8 +166,13 @@ bool iwasm_init(void)
     return tpid != 0 ? true : false;;
 }
 
+/* this seems to be a very direct interpretation of i like to have a wamr_run */
+int wamr_run(void *bytecode, size_t bytecode_len, int argc, char **argv){
+    return iwasm_bytecode_exec_main(bytecode, bytecode_len, argc, argv);
+}
+
 #define telltruth(X) ((X) ? "true" : "false")
 int main(void)
 {
-    printf("iwasm_initilised: %s\n",telltruth(iwasm_init()));
+    printf("iwasm_thread_initilised: %s\n",telltruth(iwasm_start_thread()));
 }
